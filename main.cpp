2337eb5ee79296a57511fae56d23fc1d343a1036
wny0320 <33829442+wny0320@users.noreply.github.com>
2025-03-31 16:45:29 +0900

    init

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int Number1 = 5;
+	int Number2 = 4;
+	//+, -, *, /, %
+	//이항 연산자
+	int Number3 = Number1 + Number2;
+	int Number4 = Number1 - Number2;
+	int Number5 = Number1 * Number2;
+	float Number6 = Number1 / Number2;
+	int Number7 = Number1 % Number2;
+
+
+	cout << Number3 << endl;
+	cout << Number4 << endl;
+	cout << Number5 << endl;
+	cout << Number6 << endl;
+	cout << Number7 << endl;
+	
+	return 0;
+}
